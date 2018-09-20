7b9206b05a00323f1e5baa31a214ab5a58cc6787
KwakCheol <KwakCheol0517@gmail.com>
2018-09-20 14:58:50 +0900

    방검색

---
+++ src/main/java/com/kosmo/ddabang/web/SearchController.java
@@ -0,0 +1,14 @@
+package com.kosmo.ddabang.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SearchController {
+	
+	@RequestMapping("/Search/Map.bbs")
+	public String mapList()throws Exception{	
+		
+		return "common/item/search/Map.tiles";
+	}
+}
